24b7356c4fe10a42c1f5ca4a0df7e6dfda45867f
Saputra <saputra.uta50@gmail.com>
2025-05-05 12:37:14 +0800

    Initial Commit

---
+++ SwiftDataExplore/SwiftDataExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExploreApp.swift
+//  SwiftDataExplore
+//
+//  Created by Saputra on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
